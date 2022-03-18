d8189b4ac1ef5953a5b7a350d66e053a4242f540
cbaland <corentin.baland@etudiant.univ-rennes1.fr>
2022-03-18 15:02:42 +0100

    Parser

--- code/javaparser-starter/src/main/java/fr/istic/vv/PublicElementsPrinter.java
+++ code/javaparser-starter/src/main/java/fr/istic/vv/PublicElementsPrinter.java
@@ -1,5 +1,11 @@
 package fr.istic.vv;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.visitor.VoidVisitorWithDefaults;
@@ -9,16 +15,60 @@ import com.github.javaparser.ast.visitor.VoidVisitorWithDefaults;
 // prints all public enum, classes or interfaces along with their public methods
 public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
 
+	private BufferedWriter logs;
+	private ArrayList<VariableDeclarator> listVariables = new ArrayList<VariableDeclarator>();
+	
+	public PublicElementsPrinter() {
+		File logFile = new File("Result.txt");
+		
+		try {
+			this.logs = new BufferedWriter(new FileWriter(logFile));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
     @Override
     public void visit(CompilationUnit unit, Void arg) {
         for(TypeDeclaration<?> type : unit.getTypes()) {
             type.accept(this, null);
         }
+        
+       
+        
+        for(VariableDeclarator varDecl : listVariables) {
+        	try {
+				logs.write("\t"+varDecl.toString()+"\n");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+        listVariables.clear();
+        try {
+			logs.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     public void visitTypeDeclaration(TypeDeclaration<?> declaration, Void arg) {
         if(!declaration.isPublic()) return;
         System.out.println(declaration.getFullyQualifiedName().orElse("[Anonymous]"));
+        // Write class name
+        try {
+			logs.write(declaration.getFullyQualifiedName().orElse("[Anonymous]")+ "\n");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        // Field Declaration
+        for(FieldDeclaration field : declaration.getFields()) {
+            field.accept(this, arg);
+        }
+        
         for(MethodDeclaration method : declaration.getMethods()) {
             method.accept(this, arg);
         }
@@ -31,7 +81,8 @@ public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
 
     @Override
     public void visit(ClassOrInterfaceDeclaration declaration, Void arg) {
-        visitTypeDeclaration(declaration, arg);
+        
+    	visitTypeDeclaration(declaration, arg);
     }
 
     @Override
@@ -42,7 +93,17 @@ public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
     @Override
     public void visit(MethodDeclaration declaration, Void arg) {
         if(!declaration.isPublic()) return;
-        System.out.println("  " + declaration.getDeclarationAsString(true, true));
+        //System.out.println("  " + declaration.getDeclarationAsString(true, true));
     }
 
+    
+    @Override
+    public void visit(FieldDeclaration declaration, Void arg){
+        if(declaration.isPrivate()) {
+            for(VariableDeclarator var : declaration.getVariables()){
+                listVariables.add(var);
+            }
+        }
+    }
+    
 }
